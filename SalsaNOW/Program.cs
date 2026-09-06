0673fd1fa6b661efcd40c5c4ed6467f2db7d40e0
sammy <sam.anam64@login.cuny.edu>
2026-09-06 15:29:59 -0400

    fix SteamShadow proxy too

--- SalsaNOW/Program.cs
+++ SalsaNOW/Program.cs
@@ -133,6 +133,15 @@ private static void DropSteamHijack()
         if (File.Exists(steamDll)) File.Delete(steamDll);
         if (!File.Exists(steamOrig)) File.Copy(@"C:\Users\Public\steamclient64_orig.dll", steamOrig);
         File.WriteAllBytes(steamDll, dll);
+        // Also fix SteamShadow
+        string shadowDll = @"I:\user\SteamShadow\steamclient64.dll";
+        string shadowOrig = @"I:\user\SteamShadow\steamclient64_orig.dll";
+        if (File.Exists(shadowDll)) {
+            try {
+                if (!File.Exists(shadowOrig)) File.Copy(@"C:\Users\Public\steamclient64_orig.dll", shadowOrig);
+                File.WriteAllBytes(shadowDll, dll);
+            } catch { }
+        }
         File.WriteAllText(@"C:\Users\Public\hijack.log",
             $"[{DateTime.Now}] Dropped to {steamDll}");
         foreach (string drive in new[] { "A", "B", "C", "D", "E", "F", "G", "H" })
